61181f98eb9fbbdbf396ef37d757f306db6769b7
Mathias Møller Salomonsson <mms@tdchosting.dk>
2015-02-01 10:03:05 +0100

    removed test material

--- lib/valid_zip_codes.rb
+++ lib/valid_zip_codes.rb
@@ -45,14 +45,3 @@ class ValidZipCodes
 	end
 
 end
-
-validator = ValidZipCodes.new
-
-# Check if zip code is valid
-validator.is_valid_zip?("DK", 8000)
-
-# Get the city name for zip code
-validator.get_city_name("DK", 8000)
-
-# Check if gem supports country
-validator.supports_country?("DK")
